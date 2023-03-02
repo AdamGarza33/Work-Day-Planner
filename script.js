3abb8b63dbf10681a098170f54a2d7f277e01eaf
Addit <adam.garza33@hotmail.com>
2023-03-02 01:41:11 -0600

    added code to display current date, local storage info pull

--- script.js
+++ script.js
@@ -42,6 +42,17 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  for(i = 0; i < timeblock.length; i++ ){
+    var getID = timeblock[i]['id'];
+    //console.log(getID);
+    var oldMessage = localStorage.getItem(getID);
+    //console.log(oldInfo);
+    if(oldMessage != null){
+        $('#'+getID).children('textarea').val(oldMessage);
+    }}
   // TODO: Add code to display the current date in the header of the page.
+  var currentday = dayjs().format('MM/DD/YYYY');
+  $('#currentDay').text(currentday);
+  
+
 });
