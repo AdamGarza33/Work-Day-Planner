cf3ba194f55d9f1b01fbecf113450375775b4a04
Addit <adam.garza33@hotmail.com>
2023-03-02 01:29:56 -0600

    making dayjs timestamp

--- script.js
+++ script.js
@@ -12,12 +12,15 @@ $(function () {
     var hourBlock = $(this).parent().attr('id');
     var message = $(this).prev().val();
     localStorage.setItem(hourBlock, message);
+  });
+    
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  var time = dayjs().format('HH');
+  console.log(time);
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
